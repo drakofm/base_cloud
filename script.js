29fe0d39d548589bd0a3f609550b947efa3f206d
Igor Grabeklis <drakofm@gmail.com>
2024-04-09 15:31:35 +0300

    Work (t) times harder added to master index.html

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function sayHi() {
 	alert('Hello from Git!');
 }
 
-function work() {
-	alert('Work, work harder!');
+function work(t) {
+	alert(`Work, work {$t} times harder!`);
 }
 
 function sayBye() {
